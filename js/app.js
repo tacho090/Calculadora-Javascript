de4b5f0d6246324d59636db42e1308c0994db8b4
Pablo Mora <pmora1990@gmail.com>
2019-04-01 18:34:22 -0600

    press CSS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-var button = document.getElementsByClassName("tecla");
 
 var Calculadora = {
+
+  button: function(){
+    //event handler
+    var boton = document.getElementsByClassName("tecla");
+    for (property in boton){
+    console.log(property);
+    boton[property].addEventListener("click",this.pressCSS());
+    }
+  },
+
   //change number size when pressed
   pressCSS:function(){
     //console.log(button);
@@ -32,12 +41,48 @@ var Calculadora = {
 
 };
 
+//var boton = document.getElementsByClassName("tecla")[0].id;
+
+var boton = document.getElementsByClassName("tecla");//declaracion de objeto boton
+for (property in boton){
+  console.log(boton[property].id);//obtener id de cada boton
+}
+
+function reply_click()
+{
+    alert("nueva alerta +" + event.srcElement.id);
+}
+
+/*//////////*Get element ID with click*//////////////
+
+var reply_click = function()
+{
+  alert("Button clicked, id "+this.id);
+  elem = document.getElementById(this.id);
+  elem.style.width = "50%";
+  elem.style.height = "50%";
+
+}
+document.getElementById('on').onclick = reply_click;
+document.getElementById('1').onclick = reply_click;
+document.getElementById('2').onclick = reply_click;
+document.getElementById('3').onclick = reply_click;
+document.getElementById('4').onclick = reply_click;
+document.getElementById('5').onclick = reply_click;
+document.getElementById('6').onclick = reply_click;
+document.getElementById('7').onclick = reply_click;
+document.getElementById('8').onclick = reply_click;
+document.getElementById('9').onclick = reply_click;
+document.getElementById('0').onclick = reply_click;
+
+//boton[property].addEventListener("click",this.pressCSS());
+////////////////////////////////////////////////////////*/
+
+//button.onclick = Calculadora.pressCSS;
+
 //button is an object
 //function press(){
-  for (property in button){
-    console.log(property);
-    button[property].addEventListener("click",Calculadora.pressCSS);
-  }
+
 //}
 //button[0].addEventListener("click",Calculadora.pressCSS);
 
