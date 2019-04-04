9477cc43e53db36b1b94956c33b577b0207b34cb
Pablo Mora <pmora1990@gmail.com>
2019-04-04 11:16:34 -0600

    identify: function(){}

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var string_num_total = "";
 
 var Calculadora = {
 
@@ -17,14 +18,35 @@ var Calculadora = {
     elem = document.getElementById(this.id);
     elem.style.width = "18%";
     elem.style.height = "18%";
-    this.mouseUp;
+    document.getElementById('1').value = 1;
+    console.log(this.id.value);
     Calculadora.mouseUp(this.id);
   },
   mouseUp: function(id){
     alert("Button Uppppp, id "+this.id);
     document.getElementById(id).style.width = "22%";
     document.getElementById(id).style.height = "62.91px";
+  },
+  string: function(){
+    string_num = this.id;
+    string_num_total = string_num_total + string_num;
+    console.log(string_num_total);
+  },
+  string_to_number: function(){
+
+  },
+  assignNum:function(){
+    number = document.getElementById(this.id);
+    number.value = parseInt(this.id, 10);
+  },
+  identify: function(){
+
+  },
+  sum: function(num1){
+
+    var result = num1 + this.identify();
   }
+
 
 };
 
@@ -40,7 +62,11 @@ document.getElementById('menos').onclick = Calculadora.press;
 document.getElementById('punto').onclick = Calculadora.press;
 document.getElementById('igual').onclick = Calculadora.press;
 document.getElementById('mas').onclick = Calculadora.press;
-document.getElementById('1').onclick = Calculadora.press;
+document.getElementById("mas").addEventListener("click", Calculadora.sum);
+//document.getElementById("1").addEventListener("click", Calculadora.press);
+document.getElementById("1").addEventListener("click", Calculadora.string);
+//document.getElementById("1").addEventListener("click", Calculadora.assignNum);
+//document.getElementById('1').onclick = Calculadora.press, Calculadora.asignar;
 document.getElementById('2').onclick = Calculadora.press;
 document.getElementById('3').onclick = Calculadora.press;
 document.getElementById('4').onclick = Calculadora.press;
@@ -55,11 +81,14 @@ document.getElementById('0').onclick = Calculadora.press;
 ////////////////////////////////////////////////////////*/
 /*//////////*Get element value from ID*//////////////
 
-var number = 0;
+/*var number = 0;
 number = document.getElementById('1');
 number.value = 1;
-console.log(number.value);
+console.log(number.value);*/
 
-document.getElementById('1').value = 1;
 
+/*var boton = document.getElementsByClassName("tecla");//declaracion de objeto boton
+for (property in boton){
+  console.log(boton[property].id);//obtener id de cada boton
+}*/
 ////////////////////////////////////////////////////////*/
