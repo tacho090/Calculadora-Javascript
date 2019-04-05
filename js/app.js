03ca36f2f355556d8575226c90a33dc688385cde
Pablo Mora <pmora1990@gmail.com>
2019-04-05 10:19:44 -0600

    operation sum succesful

--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
 var string_num_total = "";
-var boolean = true;
+var boolean = 0;
 var strtot = [];
+var oldNum = ""; // First number
+var theNum = ""; // Current number
+var operator;
+var result;
+var display = document.getElementById('display');
 
 var Calculadora = {
-
-  /*button: function(){
-    //event handler
-    var boton = document.getElementsByClassName("tecla");
-    for (property in boton){
-    console.log(property);
-    boton[property].addEventListener("click",this.pressCSS());
-    }
-  },*/
-
   //change number size when pressed
   press:function(){
-    //console.log(button);
-    //alert("Button clicked, id "+this.id);
+
     elem = document.getElementById(this.id);
     elem.style.width = "18%";
     elem.style.height = "18%";
@@ -29,60 +23,92 @@ var Calculadora = {
     document.getElementById(id).style.width = "22%";
     document.getElementById(id).style.height = "62.91px";
   },
-  string1: function(i, id){
+
+  //store theNum
+  string1: function(id){
 
     console.log("entered string1");
     string_num = id;
-    string_num_total = string_num_total + string_num;
+    theNum = theNum + string_num;
+    display.innerHTML = theNum;
+    return theNum
+  },
+  //store oldNum
+  moveNum: function(string){
+    string = string.toString()
+    console.log("oldNUm: " + string);
+    oldNum = string;
 
-    const ret = strtot.slice(i);
-    ret[i] = string_num_total;
+    theNum= " ";
+    console.log("newNum: " + theNum);
 
-    console.log(ret[i]);
-
-    return ret[i];
+    return oldNum;
   },
 
   identify: function(id){
-    //id = this.id;
-    //number = this.id;
-    //console.log("ID: " + this.id);
-    Calculadora.string1(id, i);
-    /*if(Calculadora.boolean(boolean) === true){
-      Calculadora.string1(id);
-    }else{
-      Calculadora.string2(id);
-    }*/
-    //Calculadora.assignNum(number);
-
+    Calculadora.string1(id);
   },
-  sum: function(num1){
-    console.log(Calculadora.string1 + " y " + Calculadora.string2)
-    //var result = Calculadora.string1 + Calculadora.string2;
+
+  operations: function(operator, oldNum, theNum){
+    console.log("Entered operations");
+    //Convert string to float
+    oldNum = parseFloat(oldNum);
+    //console.log(oldNum);
+    theNum = parseFloat(theNum);
+    //console.log(theNum);
+    switch(operator){
+      case "mas":
+        result = oldNum + theNum;
+        break;
+      case "menos":
+        result = oldNum - theNum;
+        break;
+      case "por":
+        result = oldNum * theNum;
+        break;
+      case "dividido":
+        result = oldNum / theNum;
+        break;
+      default:
+        result = theNum;
+    }
+    display.innerHTML = result;
+    theNum = result;
+    Calculadora.moveNum(theNum);
+    //return theNum;
+  },
+
+  clear: function() {
+    oldNum = "";
+    theNum = "";
+    display.innerHTML = "0";
   }
 
 
 };
-//replace items in array
-function replaceAt(array, index, value) {
-  const ret = array.slice(0);
-  ret[index] = value;
-  return ret;
-}
-/*Nuevas lineas de codigo*/
+
+
 /*//////////*Get element ID with click*//////////////
 
-document.getElementById('on').onclick = Calculadora.press;
+//get display
+document.getElementById('display').onclick = Calculadora.press;
+//
+
+document.getElementById('on').addEventListener("click", function(event){
+  Calculadora.clear;
+});
 document.getElementById('sign').onclick = Calculadora.press;
 document.getElementById('raiz').onclick = Calculadora.press;
 document.getElementById('dividido').onclick = Calculadora.press;
 document.getElementById('por').onclick = Calculadora.press;
 document.getElementById('menos').onclick = Calculadora.press;
 document.getElementById('punto').onclick = Calculadora.press;
-document.getElementById('igual').onclick = Calculadora.press;
-document.getElementById('mas').onclick = Calculadora.press;
+document.getElementById('igual').addEventListener("click", function(event){
+  Calculadora.operations(operator, oldNum, theNum);
+});
 document.getElementById("mas").addEventListener("click", function(event){
-  Calculadora.boolean(false);
+  operator = this.id;//guarda id de operador para luego usarlo en funcion operations dentro de obj Calculadora
+  Calculadora.moveNum(theNum);
 });
 //document.getElementById("1").addEventListener("click", Calculadora.press);
 document.getElementById("1").addEventListener("click", function(event){
@@ -92,15 +118,33 @@ document.getElementById("1").addEventListener("click", function(event){
 //document.getElementById("1").addEventListener("click", Calculadora.string1);
 //document.getElementById("1").addEventListener("click", Calculadora.assignNum);
 //document.getElementById('1').onclick = Calculadora.press, Calculadora.asignar;
-document.getElementById('2').onclick = Calculadora.press;
-document.getElementById('3').onclick = Calculadora.press;
-document.getElementById('4').onclick = Calculadora.press;
-document.getElementById('5').onclick = Calculadora.press;
-document.getElementById('6').onclick = Calculadora.press;
-document.getElementById('7').onclick = Calculadora.press;
-document.getElementById('8').onclick = Calculadora.press;
-document.getElementById('9').onclick = Calculadora.press;
-document.getElementById('0').onclick = Calculadora.press;
+document.getElementById('2').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('3').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('4').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('5').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('6').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('7').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('8').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('9').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
+document.getElementById('0').addEventListener("click", function(event){
+  Calculadora.identify(this.id);
+});
 
 
 ////////////////////////////////////////////////////////*/
